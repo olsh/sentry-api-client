728bed569ffd2f053b28985b75c2a3215e0a1252
Oleg Shevchenko <olsh.me@gmail.com>
2019-09-20 00:32:36 +0300

    Add properties to sentry api client

--- src/SentryApi.Client/ISentryApiClient.cs
+++ src/SentryApi.Client/ISentryApiClient.cs
@@ -4,8 +4,36 @@ namespace SentryApi.Client
 {
     public interface ISentryApiClient
     {
+        /// <summary>
+        /// Gets the events.
+        /// </summary>
+        /// <value>
+        /// The events.
+        /// </value>
+        IEventService Events { get; }
+
+        /// <summary>
+        /// Gets the projects.
+        /// </summary>
+        /// <value>
+        /// The projects.
+        /// </value>
+        IProjectService Projects { get; }
+
+        /// <summary>
+        /// Loads the next page asynchronous.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="collection">The collection.</param>
+        /// <returns>The next page.</returns>
         Task<PagedCollection<T>> LoadNextPageAsync<T>(PagedCollection<T> collection);
 
+        /// <summary>
+        /// Loads the previous page asynchronous.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <param name="collection">The collection.</param>
+        /// <returns>The next page.</returns>
         Task<PagedCollection<T>> LoadPreviousPageAsync<T>(PagedCollection<T> collection);
     }
 }
